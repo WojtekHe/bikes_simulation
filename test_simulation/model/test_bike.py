0547da6b10d5062f689f014285b7235b9ef9ae90
Wojciech Hedloff <wojhed@gmail.com>
2020-04-16 22:42:37 +0200

    delete useless test bike method

--- test_simulation/model/test_bike.py
+++ test_simulation/model/test_bike.py
@@ -5,16 +5,6 @@ from src.model.bike import Bike
 class BikeTest(unittest.TestCase):
 
     def test_bike_id(self):
-        b1 = Bike()
-        b2 = Bike()
-
-        expected_b1_id = 1
-        expected_b2_id = 2
-
-        actual_b1_id = b1.bike_id
-        actual_b2_id = b2.bike_id
-
-        self.assertEqual(expected_b1_id, actual_b1_id)
-        self.assertEqual(expected_b2_id, actual_b2_id)
+        pass
 
 
